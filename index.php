8fd0579442ac403d7797ed8ede910ce266b6634b
Chris Connor <cjconnor24@connordesign.com>
2017-11-20 10:03:59 +0000

    Forming a basic site structure in HTML along with some base styles

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Lomond Outdoor Adventures</title>
+    <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+<div>
+
+<nav>
+    <div class="container">
+    <a href="#" class="nav__brand"><span class="nav__brand__span">Lomond Adventures</span></a>
+    <ul class="nav__menu">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Facilites</a></li>
+        <li><a href="#">Activities</a></li>
+    </ul>
+    </div>
+</nav>
+<header></header>
+
+<div class="body">
+
+    <div class="container">
+<?php include('ipsum.php');?></div>
+
+</div>
+
+<footer>
+    <div class="container">
+
+    </div>
+
+    <p>Lomond Outdoor Adventures <?php echo date("Y");?> &copy;</p>
+
+</footer>
+</div>
+</body>
+</html>
