1868fc84c413e284515aab37276c0585942627f4
Chris Connor <cjconnor24@connordesign.com>
2017-11-21 14:14:36 +0000

    Lots of work on Hero and Footer
    Made good progress. Lots of work on the gero section - gradient background, expands when more text is added. Created a few button classes for re-use. Lots of work on footer - added social media menu, newsletter signup sections too.

--- index.php
+++ index.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <title>Lomond Outdoor Adventures</title>
-    <link rel="stylesheet" href="/css/styles.css">
+
     <link rel="shortcut icon" href="/favicon.ico">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/styles.css">
 </head>
 <body>
 <div>
@@ -25,8 +27,12 @@
     <div class="hero">
 
         <div class="container">
+
             <h1 class="hero__header">Welcome to Lomond Adventures</h1>
             <p class="hero__p">The number one adventure location.</p>
+            <p><a href="#" class="btn btn-orng">FIND OUT MORE</a></p>
+            <p><a href="#" class="btn btn-lg btn-orng">FIND OUT MORE</a></p>
+
         </div>
 
     </div>
@@ -39,16 +45,9 @@
 
 <div class="container overlap">
 
-    <div class="row">
-        <div class="col-4"><h1>HELLO</h1>
-        <p>THIS IS SOME CONTENT FOR COLUMN ONE. THIS IS SOME CONTENT FOR COLUMN ONE. THIS IS SOME CONTENT FOR COLUMN ONE. THIS IS SOME CONTENT FOR COLUMN ONE. </p></div>
-
-    <div class="col-2"><h1>HELLO</h1>
-        <p>THIS IS SOME CONTENT FOR COLUMN ONE. THIS IS SOME CONTENT FOR COLUMN ONE. THIS IS SOME CONTENT FOR COLUMN ONE. THIS IS SOME CONTENT FOR COLUMN ONE. </p></div>
-</div>
-
 <?php include('ipsum.php');?>
 <?php include('ipsum.php');?>
+
 </div>
 
 </div>
@@ -82,21 +81,41 @@
                 <li><a href="#">Contact Us</a></li>
             </ul></div>
         <div class="col-3"><h2>Social Media</h2>
-            <ul class="nav__menu">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Facilites</a></li>
-                <li><a href="#">Activities</a></li>
-                <li><a href="#">Contact Us</a></li>
-            </ul></div>
+
+            <ul class="sm-menu">
+                <li><a href="#"><i class="fa fa-linkedin"></i> <span>LinkedIn</span></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i> <span>Twitter</span></a></li>
+                <li><a href="#"><i class="fa fa-facebook"></i> <span>Facebook</span></a></li>
+                <li><a href="#"><i class="fa fa-instagram"></i> <span>Instagram</span></a></li>
+            </ul>
+
+            <div class="newsletter">
+                <h2>Newsletter</h2>
+                <p>Keep up-to-date with our events</p>
+                <form action="" method="post">
+
+                    <div>
+                        <label for="email">Email</label>
+                        <div>
+                        <input type="text" placeholder="Enter your email" />
+                        <button type="submit">Sign Up</button>
+                        </div>
+                    </div>
+
+                </form>
+
+            </div>
+
+        </div>
 
     </div>
 
     </div>
 
-    <p>Lomond Outdoor Adventures <?php echo date("Y");?> &copy;</p>
+    <p class="footer__copyright">Lomond Outdoor Adventures <?php echo date("Y");?> &copy;</p>
 
 </footer>
+
 </div>
 </body>
 </html>
