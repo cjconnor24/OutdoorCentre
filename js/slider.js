faba68567148844fe87d71e4f40bf01937a26759
Chris Connor <cjconnor24@connordesign.com>
2017-11-21 14:56:02 +0000

    Separating temp slider into separate file and updating

---
+++ js/slider.js
@@ -0,0 +1,44 @@
+
+/**
+ * CRUDE FUNTION TO CHANGE HERO HEADER IMAGE
+ */
+var i = 0;
+
+
+setInterval(changeHeroImage, 5000);
+
+
+
+
+function changeHeroImage(){
+
+    var loop = 0;
+
+
+
+    try {
+        console.log('THIS RAN');
+        var heroBox = document.getElementsByClassName('hero')[0];
+        var imagePath = "/img/";
+        var heroImages = [
+            "placeholder-2.jpg",
+            "placeholder-3.jpg",
+            "placeholder-4.jpg",
+            "placeholder-1.jpg"
+        ];
+
+        heroBox.style.backgroundImage = 'url(' + imagePath + heroImages[i] + ')';
+
+        if (i == 3) {
+            i = 0;
+        } else {
+            i++;
+        }
+
+    } catch (err) {
+        Console.log('REMEBER AND AMEND YOUR JS SLIDER STUFF');
+    }
+
+
+
+}
