45fd3ca9e4af0dc7230bd1fc340b98ad8052514b
Chris Connor <cjconnor24@connordesign.com>
2017-11-26 15:56:08 +0000

    Separating into separate files for re-use

---
+++ includes/footer.php
@@ -0,0 +1,81 @@
+</div>
+
+<footer>
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-4 col-m-6">
+                <h2>Contact Us</h2>
+                <ul class="footer__menu">
+                    <li><a href="#"><i class="fa fa-phone"></i> 0141 123 1234</a></li>
+                    <li><a href="#"><i class="fa fa-envelope"></i> email@lomondoutdoors.com</a></li>
+                    <li><a href="#"><i class="fa fa-map"></i> 123 Lomond Street, Loch Lomond</a></li>
+                </ul>
+
+            </div>
+
+            <div class="col-2 col-m-6">
+                <h2>Facilities</h2>
+                <ul class="footer__menu">
+                    <?php
+                    $facilities = array('Boat Hire','Equipment Hire','Life Jacket Hire');
+                    foreach($facilities as $facility){
+                        echo "<li><a href=\"#\">$facility</a></li>";
+                    }
+                    ?>
+                </ul>
+            </div>
+            <div class="col-2 col-m-6"><h2>Activities</h2>
+                <ul class="footer__menu">
+                    <?php
+                    $activities = array('Hiking','Kayaking','Canoeing','Climbing','Sailing');
+                    foreach($activities as $facility){
+                        echo "<li><a href=\"#\">$facility</a></li>";
+                    }
+                    ?>
+                </ul></div>
+
+            <div class="col-4 col-m-6"><h2>Social Media</h2>
+
+                <ul class="footer__socialmenu">
+                    <li><a href="#"><i class="fa fa-linkedin"></i> <span>LinkedIn</span></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i> <span>Twitter</span></a></li>
+                    <li><a href="#"><i class="fa fa-facebook"></i> <span>Facebook</span></a></li>
+                    <li><a href="#"><i class="fa fa-instagram"></i> <span>Instagram</span></a></li>
+                </ul>
+
+                <div class="newsletter">
+                    <h2>Newsletter</h2>
+                    <p>Keep up-to-date with our events</p>
+                    <form method="post">
+
+                        <div>
+                            <label for="email">Email</label>
+                            <div>
+                                <input type="text" id="email" name="email" placeholder="Enter your email" />
+                                <button type="submit"><i class="fa fa-pencil-square-o"></i> Sign Up</button>
+                            </div>
+                        </div>
+
+                    </form>
+
+                </div>
+
+            </div>
+
+
+
+
+        </div>
+
+    </div>
+
+    <p class="footer__copyright">Lomond Outdoor Adventures <?php echo date("Y");?> &copy;</p>
+
+</footer>
+
+</div>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
+    <title>Lomond Outdoor Adventures</title>
+
+    <link rel="shortcut icon" href="favicon.ico">
+    <!--    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">-->
+    <script
+        src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g="
+        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/styles.css" media="screen">
+
+    <script type="text/javascript" src="/js/slider.js"></script>
+    <script type="text/javascript">
+        // SHOW AND HIDE MOBILE MENU - ANIMATE HAMBURGER
+        $(document).ready(function(){
+            $('.nav__hamburger').click(function(){
+                $(this).toggleClass('open');
+                $(".nav__menu").toggleClass( "open" );
+            });
+        });
+    </script>
+</head>
+<body>
+
+<!--EU COOKIE LAW DISCLAIMER-->
+<!--<div class="cookies">-->
+<!--    <p class="cookies__message">This site used cookies blah blah blah. <a href="#" class="cookies__link-agree">I agree.</a> | <a href="#" class="cookies__link-disagree">I do not agree.</a></p>-->
+<!--</div>-->
+
+<div>
+    <header>
+        <nav>
+            <div class="container">
+                <a href="#" class="nav__brand"><span class="nav__brand__span">Lomond Adventures</span></a>
+
+                <?php include('includes/navigation.php'); ?>
+
+                <div class="nav__telephone">
+                    <a href="tel:01236123456"><i class="fa fa-phone"></i></a>
+                </div>
+
+                <div class="nav__hamburger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+
+            </div>
+        </nav>
+
+        <div class="hero">
+
+            <div class="container">
+
+                <h1 class="hero__header">Welcome to Lomond Adventures</h1>
+                <p class="hero__p">The number one adventure location.</p>
+                <p><a href="#" class="btn btn-orng">FIND OUT MORE</a></p>
+                <p><a href="#" class="btn btn-lg btn-orng">FIND OUT MORE</a></p>
+
+            </div>
+
+        </div>
+
+    </header>
+
+
+
+    <div class="body">
