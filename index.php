057dc73684644df360b575429609f88c93ca2850
Chris Connor <cjconnor24@connordesign.com>
2017-11-26 14:18:13 +0000

    Added mega menu
    Created a mega menu to hold the sub sections of the activities and the facilities

--- index.php
+++ index.php
@@ -43,14 +43,24 @@
     <ul class="nav__menu open">
         <li><a href="#">Home</a></li>
         <li><a href="#">About Us</a></li>
-        <li><a href="#">Facilites</a></li>
+        <li><a href="#">Facilites</a>
+            <ul class="nav__submenu">
+                <li><a href="#"><img src="/img/placeholder-1.jpg" alt="Placeholder">First</a></li>
+                <li><a href="#"><img src="/img/placeholder-1.jpg" alt="Placeholder">Climbing</a></li>
+                <li><a href="#"><img src="/img/placeholder-1.jpg" alt="Placeholder">Canoeing</a></li>
+                <li><a href="#"><img src="/img/placeholder-1.jpg" alt="Placeholder">Walking</a></li>
+                <li><a href="#"><img src="/img/placeholder-1.jpg" alt="Placeholder">Canoeing</a></li>
+                <li><a href="#"><img src="/img/placeholder-1.jpg" alt="Placeholder">Walking</a></li>
+            </ul>
+        </li>
         <li><a href="#">Activities</a>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Facilites</a></li>
-                <li><a href="#">Activities</a></li>
-                <li><a href="#">Contact Us</a></li>
+            <ul class="nav__submenu">
+                <li><a href="#"><img src="/img/placeholder-1.jpg" alt="Placeholder">Second</a></li>
+                <li><a href="#"><img src="/img/placeholder-1.jpg" alt="Placeholder">Climbing</a></li>
+                <li><a href="#"><img src="/img/placeholder-1.jpg" alt="Placeholder">Canoeing</a></li>
+                <li><a href="#"><img src="/img/placeholder-1.jpg" alt="Placeholder">Walking</a></li>
+                <li><a href="#"><img src="/img/placeholder-1.jpg" alt="Placeholder">Canoeing</a></li>
+                <li><a href="#"><img src="/img/placeholder-1.jpg" alt="Placeholder">Walking</a></li>
             </ul>
         </li>
         <li><a href="#">Contact Us</a></li>
