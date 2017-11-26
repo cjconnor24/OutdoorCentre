6407a19ddefd5315d9560cc25a0bc4bd3019daa8
Chris Connor <cjconnor24@connordesign.com>
2017-11-26 16:27:55 +0000

    adding about us page

---
+++ about-us.php
@@ -0,0 +1,35 @@
+<?php include('includes/header.php');?>
+
+<div class="hero">
+
+    <div class="container">
+
+        <h1 class="hero__header">Welcome to Lomond Adventures</h1>
+        <p class="hero__p">The number one adventure location.</p>
+
+        <p><a href="#" class="btn btn-lg btn-orng">FIND OUT MORE</a></p>
+
+    </div>
+
+</div>
+
+
+    <div class="container overlap">
+        <?php include('ipsum.php');?>
+    </div>
+
+    <div class="band">
+        <div class="container">
+            <?php include('ipsum.php');?>
+
+        </div>
+
+    </div>
+
+    <div class="container">
+        <?php include('ipsum.php');?>
+    </div>
+
+<?php include('includes/footer.php'); ?>
+
+
