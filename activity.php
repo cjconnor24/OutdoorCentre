dff60f8c86ac214943d1dc238407bf8bc28cb693
Chris Connor <cjconnor24@connordesign.com>
2017-12-17 14:18:46 +0000

    Activity Page Fixes
    Spent a bit more time on getting the activity listing page to look good. Still need to take a look at the mobile element

--- activity.php
+++ activity.php
@@ -9,7 +9,8 @@
 
     <div class="container overlap">
 
-        <h1>Activites</h1>
+        <h1 class="heading">Activites</h1>
+
 
         <?php
         $activities = array("Kayaking","Canoeing","Climbing","Walking");
@@ -22,10 +23,17 @@
             <img src="/img/placeholder-<?php echo $i+1;?>.jpg" alt="<?php echo $activities[$i];?>">
             </div>
 
+            <div class="activity__content">
             <h2 class="activity__heading"><?php echo $activities[$i];?></h2>
             <p>Kayaking is absolutely brilliant fun and very good fun to do. You can do lots of kayaking and some dummy text just to pad this out.</p>
+                <?php if($i==3){
+                   echo "<p>Kayaking is absolutely brilliant fun and very good fun to do. You can do lots of kayaking and some dummy text just to pad this out.</p>";
+                }?>
             <p><a href="#" class="activity__button">More Info <i class="fa fa-info-circle"></i></a></p>
+            </div>
+
         </div>
+
         <?php } ?>
 
 
