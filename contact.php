3a4565284913b59e80d216a6a8f6b3b9b6c55e35
Chris Connor <cjconnor24@connordesign.com>
2017-12-17 14:45:53 +0000

    Added map to contact page

--- contact.php
+++ contact.php
@@ -1,22 +1,157 @@
 <?php include('includes/header.php');?>
 
-<div class="hero">
-
-    <div class="container">
-
-        <h1 class="hero__header">Welcome to Lomond Adventures</h1>
-        <p class="hero__p">The number one adventure location.</p>
-
-        <p><a href="#" class="btn btn-lg btn-orng">FIND OUT MORE</a></p>
-
-    </div>
-
+<!--<div class="hero">-->
+<!---->
+<!--    <div class="container">-->
+<!---->
+<!--        <h1 class="hero__header">Welcome to Lomond Adventures</h1>-->
+<!--        <p class="hero__p">The number one adventure location.</p>-->
+<!---->
+<!--        <p><a href="#" class="btn btn-lg btn-orng">FIND OUT MORE</a></p>-->
+<!---->
+<!--    </div>-->
+<!---->
+<!--</div>-->
+<div id="location-map">
+<h1>TEST</h1>
 </div>
+
+<script>
+    function initMap() {
+
+        var style = {
+            retro: [
+            {elementType: 'geometry', stylers: [{color: '#ebe3cd'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#523735'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#f5f1e6'}]},
+            {
+                featureType: 'administrative',
+                elementType: 'geometry.stroke',
+                stylers: [{color: '#c9b2a6'}]
+            },
+            {
+                featureType: 'administrative.land_parcel',
+                elementType: 'geometry.stroke',
+                stylers: [{color: '#dcd2be'}]
+            },
+            {
+                featureType: 'administrative.land_parcel',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#ae9e90'}]
+            },
+            {
+                featureType: 'landscape.natural',
+                elementType: 'geometry',
+                stylers: [{color: '#dfd2ae'}]
+            },
+            {
+                featureType: 'poi',
+                elementType: 'geometry',
+                stylers: [{color: '#dfd2ae'}]
+            },
+            {
+                featureType: 'poi',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#93817c'}]
+            },
+            {
+                featureType: 'poi.park',
+                elementType: 'geometry.fill',
+                stylers: [{color: '#a5b076'}]
+            },
+            {
+                featureType: 'poi.park',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#447530'}]
+            },
+            {
+                featureType: 'road',
+                elementType: 'geometry',
+                stylers: [{color: '#f5f1e6'}]
+            },
+            {
+                featureType: 'road.arterial',
+                elementType: 'geometry',
+                stylers: [{color: '#fdfcf8'}]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'geometry',
+                stylers: [{color: '#f8c967'}]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'geometry.stroke',
+                stylers: [{color: '#e9bc62'}]
+            },
+            {
+                featureType: 'road.highway.controlled_access',
+                elementType: 'geometry',
+                stylers: [{color: '#e98d58'}]
+            },
+            {
+                featureType: 'road.highway.controlled_access',
+                elementType: 'geometry.stroke',
+                stylers: [{color: '#db8555'}]
+            },
+            {
+                featureType: 'road.local',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#806b63'}]
+            },
+            {
+                featureType: 'transit.line',
+                elementType: 'geometry',
+                stylers: [{color: '#dfd2ae'}]
+            },
+            {
+                featureType: 'transit.line',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#8f7d77'}]
+            },
+            {
+                featureType: 'transit.line',
+                elementType: 'labels.text.stroke',
+                stylers: [{color: '#ebe3cd'}]
+            },
+            {
+                featureType: 'transit.station',
+                elementType: 'geometry',
+                stylers: [{color: '#dfd2ae'}]
+            },
+            {
+                featureType: 'water',
+                elementType: 'geometry.fill',
+                stylers: [{color: '#b9d3c2'}]
+            },
+            {
+                featureType: 'water',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#92998d'}]
+            }
+        ]};
+
+        var lochlomond = {lat: 56.085865, lng: -4.548176};
+        var map = new google.maps.Map(document.getElementById('location-map'), {
+            zoom: 14,
+            center: lochlomond,
+            disableDefaultUI: true
+        });
+        map.setOptions({styles: style['retro']});
+        var marker = new google.maps.Marker({
+            position: lochlomond,
+            map: map
+        });
+    }
+</script>
+<script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAbs8Wb7fnp6cMbiuuWfJbX-3X3ItHC2Rw&callback=initMap">
+</script>
 
 
 <div class="container overlap">
 
-    <h1>Get in Touch!</h1>
+    <h1 class="heading">Get in Touch!</h1>
 
     <p>To get in contact with us, simply fill in the form below. Alternatively you can email us.</p>
 
