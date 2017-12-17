c3dd4f0d8694f827f5c7530db45b54a2f217bee7
Chris Connor <cjconnor24@connordesign.com>
2017-12-17 20:05:15 +0000

    Adding mailing list to nav

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -1,5 +1,6 @@
 <li><a href="/admin/"><i class="fa fa-home"></i> <span>Dashboard</span></a></li>
 <li><a href="/admin/enquiries.php"><i class="fa fa-envelope"></i> <span>Enquiries</span></a></li>
 <li><a href="#"><i class="fa fa-map"></i> <span>Routes</span></a></li>
+<li><a href="/admin/newsletter.php"><i class="fa fa-sign-in"></i> <span>Newsletter</span></a></li>
 <li><a href="#"><i class="fa fa-gears"></i> <span>Settings</span></a></li>
 <li><a href="/"><i class="fa fa-sign-out"></i> <span>Logout</span></a></li>
