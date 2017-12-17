433019a8ed11593721042f576095629d01629b05
Chris Connor <cjconnor24@connordesign.com>
2017-12-17 19:29:25 +0000

    Tweaking the table appearance and adding email anchors

--- admin/enquiries.php
+++ admin/enquiries.php
@@ -6,22 +6,22 @@
 <table class="table">
         <thead>
         <tr>
-                <th>#</th>
+                <th># Enquiry Ref</th>
                 <th>Name</th>
-                <th>Data</th>
-                <th>Buttons</th>
+                <th>Date Submitted</th>
+                <th>Actions</th>
         </tr>
         </thead>
         <tbody>
         <?php for($i = 0;$i<300;$i++){?>
         <tr>
                 <td>#1234</td>
-                <td><strong>Christopher Connor </strong><br><em>Another One</em></td>
-                <td>11/12/2017</td>
+                <td><strong>Christopher Connor </strong><br><em><a href="#">cjconnor24@hotmail.com</a></em></td>
+                <td><i class="fa fa-calendar"></i> 11/12/2017</td>
                 <td>
-                        <a href="#" class="table__button"><i class="fa fa-envelope-open"></i> <span>Read</span></a>
-                        <a href="#"class="table__button"><i class="fa fa-reply"></i> <span>Read</span></a>
-                        <a href="#" class="table__button"><i class="fa fa-trash"></i> <span>Read</span></a>
+                        <a href="#" title="View Enquiry" class="table__button"><i class="fa fa-envelope-open"></i> <span>Read</span></a>
+                        <a href="#"title="Reply to Enquiry" class="table__button"><i class="fa fa-reply"></i> <span>Read</span></a>
+                        <a href="#" title="Delete Enquiry" class="table__button"><i class="fa fa-trash"></i> <span>Read</span></a>
                 </td>
         </tr>
         <?php } ?>
