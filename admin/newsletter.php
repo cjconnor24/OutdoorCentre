878b74209a283dcb3757efa3b54dd8d2fa2fe87f
Chris Connor <cjconnor24@connordesign.com>
2017-12-17 20:04:54 +0000

    Adding newsletter page to show who is signed up for mailing list.

---
+++ admin/newsletter.php
@@ -0,0 +1,54 @@
+<?php include('includes/header.php'); ?>
+
+ <h1>Enquiries</h1>
+
+<?php
+include('../includes/dbConnect.php');
+
+$query = $conn->prepare("SELECT * from newsletter;");
+
+$query->execute();
+$count = $query->rowCount();
+
+if($count > 0) {
+    ?>
+
+    <p>Below are the current users signed up to the mailing list.</p>
+
+    <table class="table">
+        <thead>
+        <tr>
+            <th># User Id</th>
+            <th>Email</th>
+            <td>Actions</td>
+        </tr>
+        </thead>
+        <tbody>
+    <?php
+    $results = $query->fetchAll(PDO::FETCH_ASSOC);
+    foreach($results as $row){
+    ?>
+            <tr>
+                <td>#<?php echo $row['id'];?></td>
+                <td><strong><a href="mailto:<?php echo $row['email'];?>"><?php echo $row['email'];?></a></strong><br><em></a></em></td>
+                <td>
+                    <a href="mailto:<?php echo $row['email'];?>" title="Email User" class="table__button"><i class="fa fa-envelope"></i>
+                        <span>Read</span></a>
+                    <a href="#<?php echo $row['id'];?>" title="Remove From Mailing List" class="table__button remove"><i class="fa fa-trash"></i>
+                        <span>Read</span></a>
+                </td>
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
+
+    <?php
+} else {
+    ?>
+    <p>There are currently no enquiries.</p>
+<?php
+}
+?>
+
+<?php include('includes/footer.php'); ?>
+
