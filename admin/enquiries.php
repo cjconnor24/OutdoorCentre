87501c40750b7c3d736212446bc2d8cc9e84c7c7
Chris Connor <cjconnor24@connordesign.com>
2017-12-17 20:05:46 +0000

    Removing hard-coded info

--- admin/enquiries.php
+++ admin/enquiries.php
@@ -1,32 +1,59 @@
 <?php include('includes/header.php'); ?>
 
  <h1>Enquiries</h1>
-<p>Below are a list of enquiries</p>
 
-<table class="table">
+<?php
+include('../includes/dbConnect.php');
+
+$query = $conn->prepare("SELECT enquiry.id, enquiry.name, enquiry.email, enquiry.created_at, activity.name AS activity FROM enquiry LEFT JOIN activity ON enquiry.activity = activity.id");
+
+$query->execute();
+$count = $query->rowCount();
+
+if($count > 0) {
+    ?>
+
+    <p>Below are a list of enquiries</p>
+
+    <table class="table">
         <thead>
         <tr>
-                <th># Enquiry Ref</th>
-                <th>Name</th>
-                <th>Date Submitted</th>
-                <th>Actions</th>
+            <th># Enquiry Ref</th>
+            <th>Name</th>
+            <th>Date Submitted</th>
+            <th>Activity</th>
+            <th>Actions</th>
         </tr>
         </thead>
         <tbody>
-        <?php for($i = 0;$i<300;$i++){?>
-        <tr>
-                <td>#1234</td>
-                <td><strong>Christopher Connor </strong><br><em><a href="#">cjconnor24@hotmail.com</a></em></td>
-                <td><i class="fa fa-calendar"></i> 11/12/2017</td>
+    <?php
+    $results = $query->fetchAll(PDO::FETCH_ASSOC);
+    foreach($results as $row){
+    ?>
+            <tr>
+                <td>#<?php echo $row['id'];?></td>
+                <td><strong><?php echo $row['name'];?></strong><br><em><a href="mailto:<?php echo $row['email'];?>"><?php echo $row['email'];?></a></em></td>
+                <td><i class="fa fa-calendar"></i> <?php echo $row['created_at'];?></td>
+                <td><strong><?php echo $row['activity'];?></strong></td>
                 <td>
-                        <a href="#" title="View Enquiry" class="table__button"><i class="fa fa-envelope-open"></i> <span>Read</span></a>
-                        <a href="#"title="Reply to Enquiry" class="table__button"><i class="fa fa-reply"></i> <span>Read</span></a>
-                        <a href="#" title="Delete Enquiry" class="table__button"><i class="fa fa-trash"></i> <span>Read</span></a>
+                    <a href="#<?php echo $row['id'];?>" title="View Enquiry" class="table__button"><i class="fa fa-envelope-open"></i> <span>Read</span></a>
+                    <a href="#<?php echo $row['id'];?>" title="Reply to Enquiry" class="table__button"><i class="fa fa-reply"></i>
+                        <span>Read</span></a>
+                    <a href="#<?php echo $row['id'];?>" title="Delete Enquiry" class="table__button remove"><i class="fa fa-trash"></i>
+                        <span>Read</span></a>
                 </td>
-        </tr>
+            </tr>
         <?php } ?>
         </tbody>
-</table>
+    </table>
+
+    <?php
+} else {
+    ?>
+    <p>There are currently no enquiries.</p>
+<?php
+}
+?>
 
 <?php include('includes/footer.php'); ?>
 
