628b5f1d88cb575dca1a90924533f8137063b12c
Chris Connor <cjconnor24@connordesign.com>
2017-12-18 00:04:45 +0000

    syntax amends

--- admin/enquiries.php
+++ admin/enquiries.php
@@ -22,14 +22,14 @@ if($count > 0) {
     ?>
 
     <p>Below are a list of enquiries</p>
-
+<!--TODO: MAKE TABLES REPONSIVE-->
     <table class="table">
         <thead>
         <tr>
             <th># Enquiry Ref</th>
             <th>Name</th>
             <th>Date Submitted</th>
-            <th>Activity</th>
+            <th class="hide">Activity</th>
             <th>Actions</th>
         </tr>
         </thead>
@@ -44,7 +44,7 @@ if($count > 0) {
                 <td>#<?php echo $row['enquiryid'];?></td>
                 <td><strong><?php echo $row['enquiryname'];?></strong><br><em><a href="mailto:<?php echo $row['email'];?>"><?php echo $row['enquiryemail'];?></a></em></td>
                 <td><i class="fa fa-calendar"></i> <?php echo $row['received'];?></td>
-                <td><strong><?php echo $row['activityname'];?></strong></td>
+                <td class="hide"><strong><?php echo $row['activityname'];?></strong></td>
                 <td>
                     <a href="view-enquiry.php?id=<?php echo $row['enquiryid'];?>" title="View Enquiry" class="table__button<?php echo ($row['responseid']==NULL ? " remove" : "");?>"><i class="fa fa-envelope<?php echo ($row['responseid']==NULL ? "" : "-open");?>"></i> <span>Read</span></a>
                     <a href="#<?php echo $row['enquiryid'];?>" title="Reply to Enquiry" class="table__button"><i class="fa fa-reply"></i> <span>Read</span></a>
