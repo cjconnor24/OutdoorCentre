af6082e0c6cdc11ca2f15b739fb506ec4322ffe0
Chris Connor <cjconnor24@connordesign.com>
2017-12-19 23:53:41 +0000

    Adding a file which will check for a authenticated user

--- admin/includes/secure.php
+++ admin/includes/secure.php
@@ -1,7 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: chrisconnor
- * Date: 19/12/2017
- * Time: 23:05
- */
+session_start();
+
+$page = urlencode($_SERVER['REQUEST_URI']);
+
+$_SESSION['redirect'] = $page;
+
+if(!isset($_SESSION['user'])){
+
+    // SESSION ISN'T SET - BOUNCE BACK TO LOGIN
+    header("Location: /admin/login.php");
+
+}
