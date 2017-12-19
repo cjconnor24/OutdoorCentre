5c5256d9aaf1f389f7f9bd03e23313aa57e4a84f
Chris Connor <cjconnor24@connordesign.com>
2017-12-19 20:30:11 +0000

    Updating links for testing

--- includes/navigation.php
+++ includes/navigation.php
@@ -5,20 +5,14 @@
         <ul class="nav__submenu">
             <li><a href="#"><img src="/img/placeholder-1.jpg" alt="Placeholder">First</a></li>
             <li><a href="#"><img src="/img/placeholder-1.jpg" alt="Placeholder">Climbing</a></li>
-            <li><a href="#"><img src="/img/placeholder-1.jpg" alt="Placeholder">Canoeing</a></li>
-            <li><a href="#"><img src="/img/placeholder-1.jpg" alt="Placeholder">Walking</a></li>
-            <li><a href="#"><img src="/img/placeholder-1.jpg" alt="Placeholder">Canoeing</a></li>
-            <li><a href="#"><img src="/img/placeholder-1.jpg" alt="Placeholder">Walking</a></li>
         </ul>
     </li>
     <li><a href="activity.php">Activities</a>
         <ul class="nav__submenu">
-            <li><a href="/activity-page.php"><img src="/img/placeholder-1.jpg" alt="Placeholder">Second</a></li>
-            <li><a href="#"><img src="/img/placeholder-1.jpg" alt="Placeholder">Climbing</a></li>
-            <li><a href="#"><img src="/img/placeholder-1.jpg" alt="Placeholder">Canoeing</a></li>
-            <li><a href="#"><img src="/img/placeholder-1.jpg" alt="Placeholder">Walking</a></li>
-            <li><a href="#"><img src="/img/placeholder-1.jpg" alt="Placeholder">Canoeing</a></li>
-            <li><a href="#"><img src="/img/placeholder-1.jpg" alt="Placeholder">Walking</a></li>
+            <li><a href="/activity-page.php?activity=5"><img src="/img/placeholder-1.jpg" alt="Placeholder">Second</a></li>
+            <li><a href="/activity-page.php?activity=6"><img src="/img/placeholder-1.jpg" alt="Placeholder">Climbing</a></li>
+            <li><a href="/activity-page.php?activity=7"><img src="/img/placeholder-1.jpg" alt="Placeholder">Canoeing</a></li>
+            <li><a href="/activity-page.php?activity=8"><img src="/img/placeholder-1.jpg" alt="Placeholder">Walking</a></li>
         </ul>
     </li>
     <li><a href="contact.php">Contact Us</a></li>
