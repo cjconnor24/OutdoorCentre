7ed5ca92adfa870ba80a09513bd187e6e259d21a
Chris Connor <cjconnor24@connordesign.com>
2017-12-19 23:53:09 +0000

    Adding a view site option

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -3,4 +3,5 @@
 <li><a href="/admin/routes.php" title="Manage Routes"><i class="fa fa-map-o"></i> <span>Routes</span></a></li>
 <li><a href="/admin/newsletter.php" title="Manage Newsletter"><i class="fa fa-pencil-square"></i> <span>Newsletter</span></a></li>
 <li><a href="#" title="Manage Settings"><i class="fa fa-gears"></i> <span>Settings</span></a></li>
-<li><a href="/" title="Logout"><i class="fa fa-sign-out"></i> <span>Logout</span></a></li>
+<li><a href="/" target="_blank" title="View Site"><i class="fa fa-eye"></i> <span>View Site</span></a></li>
+<li><a href="/admin/logout.php" title="Logout"><i class="fa fa-sign-out"></i> <span>Logout</span></a></li>
