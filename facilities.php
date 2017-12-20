d2dbfa715b99534d0dd5da946ecc41100424ee23
Chris Connor <cjconnor24@connordesign.com>
2017-12-20 08:56:34 +0000

    Adding a facility page - adding content

---
+++ facilities.php
@@ -0,0 +1,30 @@
+<?php include('includes/header.php');?>
+
+<div class="hero">
+
+    <div class="container">
+        <div class="fade-in">
+        <h1 class="hero__header">Facilities</h1>
+        <p class="hero__p">At Lomond Aventure, we have excellent facilities at our centre.</p>
+        </div>
+
+<!--        <p><a href="#" class="btn btn-lg btn-orng">FIND OUT MORE</a></p>-->
+
+    </div>
+
+</div>
+
+
+    <div class="container">
+
+        <h1>Facilities</h1>
+        <p>Here are some facilities</p>
+
+        <h2>Equipement Hire</h2>
+
+    </div>
+
+
+<?php include('includes/footer.php'); ?>
+
+
