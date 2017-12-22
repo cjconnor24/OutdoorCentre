6111d28431d30c1bd0a3460a17812dd33d1bf574
Chris Connor <cjconnor24@connordesign.com>
2017-12-22 09:31:53 +0000

    set to one day for demo

--- cookie-set.php
+++ cookie-set.php
@@ -1,7 +1,7 @@
 <?php
 // SET COOKIE
 header("Content-type: application/json");
-setcookie("cookie_agree", "agreed", time()+86400*30, "/");
+setcookie("cookie_agree", "agreed", time()+86400*1, "/"); // SET TO ONE DAY FOR DEMO PURPOSES
 $response = array('status'=>'success');
 echo json_encode($response);
 
